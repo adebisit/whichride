781581678dc18023f97115a54921d4590e24f951
Taiwo Adebisi <adebisijosephh@gmail.com>
2023-01-16 21:23:52 +0100

    Final Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SingUp from "./components/SingUp";
 
 function App() {
     return (
-        <div>
+        <>
             <Header />
             <main>
                 <section className="hero">
@@ -29,7 +29,7 @@ function App() {
                 </div>
             </main>
             <Footer />
-        </div>
+        </>
     );
 }
 
