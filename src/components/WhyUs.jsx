d33b00f29b38f3505a6736c26069ddc82d21dec8
Taiwo Adebisi <adebisijosephh@gmail.com>
2023-01-16 18:10:00 +0100

    RWD

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -6,7 +6,7 @@ import fastClockSvg from "../assets/mdi_clock-fast.svg"
 function WhyUs() {
   return (
     <div className="" style={{padding: '80px', position: 'relative'}}>
-        {/* <div style={{
+        <div style={{
             position: "absolute",
             width: '50%',
             height: '100%',
@@ -14,11 +14,11 @@ function WhyUs() {
             top: '0',
             left: '0',
             zIndex: '-1',
-        }}></div> */}
+        }}></div>
         <div className="textCenter">
             <h1 className="pageTitle">Why Whichride</h1>
         </div>
-        <div className="cards">
+        {/* <div className="cards">
             <div>
                 <div className="card">
                     <div className="svgContainer" style={{ backgroundColor: '#56CCF2'}}>
@@ -50,6 +50,36 @@ function WhyUs() {
                     <h6 className="title">More Choices</h6>
                     <p className="subtitle">WhichRide lets you compare pricing across the major rideshares so you can save money on every ride.</p>
                 </div>
+            </div>
+        </div> */}
+        <div className="cards">
+            <div className="card">
+                <div className="svgContainer" style={{ backgroundColor: '#56CCF2'}}>
+                    <img src={mapCarSvg} alt="mapCarSvg"/>
+                </div>
+                <h6 className="title">More Choices</h6>
+                <p className="subtitle">WhichRide lets you compare pricing across the major rideshares so you can save money on every ride.</p>
+            </div>
+            <div className="card">
+                <div className="svgContainer" style={{ backgroundColor: '#F2C94C'}}>
+                    <img src={fastClockSvg} alt="fastClockSvg"/>
+                </div>
+                <h6 className="title">More Choices</h6>
+                <p className="subtitle">WhichRide lets you compare pricing across the major rideshares so you can save money on every ride.</p>
+            </div>
+            <div className="card">
+                <div className="svgContainer" style={{ backgroundColor: '#6FCF97'}}>
+                    <img src={antDesignSafetySvg} alt="antDesignSafetySvg"/>
+                </div>
+                <h6 className="title">More Choices</h6>
+                <p className="subtitle">WhichRide lets you compare pricing across the major rideshares so you can save money on every ride.</p>
+            </div>
+            <div className="card">
+                <div className="svgContainer" style={{ backgroundColor: '#BB6BD9'}}>
+                    <img src={byteSizeOptionSvg} alt="byteSizeOptionSvg"/>
+                </div>
+                <h6 className="title">More Choices</h6>
+                <p className="subtitle">WhichRide lets you compare pricing across the major rideshares so you can save money on every ride.</p>
             </div>
         </div>
     </div>
