260588df3e2dcf4c1b5efafa37ce3041a8cd92e4
Taiwo Adebisi <adebisijosephh@gmail.com>
2023-01-16 17:45:57 +0100

    RWD

--- src/components/HowTo.jsx
+++ src/components/HowTo.jsx
@@ -1,40 +1,34 @@
 function HowTo() {
   return (
-    <div style={{width: '75%', margin: 'auto'}}>
-        <div className="textCenter" style={{ padding: '0 180px' }}>
-            <h1 className="pageTitle">
+    <div class="howToWrapper">
+        <div className="textCenter" style={{ maxWidth: '500px', margin: 'auto', paddingBottom: '30px'}}>
+            <h1 className="pageTitle textCenter">
                 Whichride brings together multiple ride hailing providers and local taxi firms in one app
             </h1>
-        </div>
-        <div className="textCenter" style={{ padding: '0 270px' }}>
             <p className="pageSubtitle">
                 Allowing riders to select the provider that offers the best price, service or travel time, then seamlessly book within the app
             </p>
         </div>
-        <div className="" style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around"
-        }}>
-            <div>
+        <div className="instructionListContainer">
+            <div style={{ textAlign: 'center'}}>
                 <img src="./iPhoneX2.png" alt="iPhoneX2" />
             </div>
             <ul className="instructionList">
-                <li className="instructionItem active">
+                <li className="instructionItem active" style={{ padding: '15px 0px'}}>
                     <div>Step 1</div>
                     <div>
                         <p className="title">Enter Location</p>
                         <p className="subtitle">Enter your destination to see a list of taxi providers near you.</p>
                     </div>
                 </li>
-                <li className="instructionItem">
+                <li className="instructionItem" style={{ padding: '15px 0px'}}>
                     <div>Step 2</div>
                     <div>
                         <p className="title">Find the perfect ride</p>
                         <p className="subtitle">Filter results by driver rating, driver sex, waiting times, price or speed. Reduce carbon footprint by only showing hybrid or electric cars.</p>
                     </div>
                 </li>
-                <li className="instructionItem">
+                <li className="instructionItem" style={{ padding: '15px 0px'}}>
                     <div>Step 2</div>
                     <div>
                         <p className="title">Pay and Save</p>
