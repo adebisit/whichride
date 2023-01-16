87854cf5516d49c77ee1ffd8e28dea1e0841caac
Taiwo Adebisi <adebisijosephh@gmail.com>
2023-01-16 23:33:26 +0100

    Full Nav

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -12,34 +12,29 @@ function Header() {
                 <img src="./logo.svg" width="150px" alt="which-ride"/>
             </div>
             <div>
-                <button id="expandMenu" onClick={() => setMenuVisibility(prev => !prev)}>
-                    {menuVisibility ? (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.5rem', height: '1.5rem' }}>
+                <button id="showMenu" onClick={() => setMenuVisibility(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.25rem', height: '1.25rem' }}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
+                <div className={`navbar ${menuVisibility ? 'show' : 'hide'}`}>
+                    <button id="hideMenu" onClick={() => setMenuVisibility(false)} style={{color: "white"}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.25rem', height: '1.25rem' }}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
-                    ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ width: '1.5rem', height: '1.5rem' }}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                    )}
-                </button>
-{/* 
-                <Menu>
-                    <a id="home" className="menu-item" href="/">Home</a>
-                    <a id="about" className="menu-item" href="/about">About</a>
-                    <a id="contact" className="menu-item" href="/contact">Contact</a>
-                </Menu> */}
-                <ul className={`navbar ${menuVisibility ? 'show' : 'hide'}`}>
-                    <li className="navbarItem">
-                        <a href="/">The App</a>
-                    </li>
-                    <li className="navbarItem">
-                        <a href="/">About Us</a>
-                    </li>
-                    <li className="navbarItem">
-                        <a href="/">Get In Touch</a>
-                    </li>
-                </ul>
+                    </button>
+                    <ul>
+                        <li className="navbarItem">
+                            <a href="/">The App</a>
+                        </li>
+                        <li className="navbarItem">
+                            <a href="/">About Us</a>
+                        </li>
+                        <li className="navbarItem">
+                            <a href="/">Get In Touch</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </header>
     )
